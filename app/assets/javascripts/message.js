bddb86dc2a82fea7634c9a8001887315c88e8f5a
saito-masanao <saitoumasanao@saitoumasanaonoMacBook-Pro.local>
2019-03-15 16:24:33 +0900

    add_HTML_to__message_index

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,24 +3,29 @@ $(function(){
     var html = `<div class = "message">
                   <div class = "upper-message">
                     <div class = "upper-message__user-name">
-                      $(message.user_name)
+                      ${message.user_name}
                     </div>
                     <div class = "upper-message__date">
-                      $(message.created_time)
+                      ${message.created_time}
                     </div>
+                  </div>
                   <div class = "lower-meesage">
-                    <% if message.content.present? %>
-                      <p class = "lower-message__content">
-                        $(message.text)
-                      </p>
-                    <% end %>
-                    <% if message.image.present? %>
-                      <%= image_tag message.image.url, class: 'lower-message__image' %>
-                    <% end %>
-                  </div>`
+                  </div>
+                </div>`
     return html;
   }
-  $('#form__message').on('submit', function(e){
+  function buildHTML_message(message){
+    var html =`<p class = "lower-message__content">
+                ${message.text}
+              </p>`
+    return html;
+  }
+  function buildHTML_img(message){
+    var html =`<img src = ${message.image_url}, class = 'lower-message__image' >`
+    return html;
+  }
+
+  $('#form__message__img').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -30,12 +35,23 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
+      contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
+      var html_message = buildHTML_message(data);
+      var html_img = buildHTML_img(data);
       $('.messages').append(html)
+      if( ${message.text} !== null){
+        $('.lower-meesage').append(html_message)
+      }
+      if( ${message.image_url} !== null){
+        $('.lower-meesage').append(html_img)
+      }
       $('.form__message').val('')
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 })
