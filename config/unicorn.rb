67b94826fe27383832c8edacc603826f1e26cb3a
saito-masanao <saitoumasanao@saitoumasanaonoMacBook-Pro.local>
2019-04-23 14:51:56 +0900

    Correct config/environments/production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,3 @@
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../", __FILE__), "current", "Gemfile")
-end
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
